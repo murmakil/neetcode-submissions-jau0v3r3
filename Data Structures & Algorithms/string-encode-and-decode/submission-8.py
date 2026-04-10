491784d59536a4bf10fd37ac86c81a693121de3b
murmakil <100516945+murmakil@users.noreply.github.com>
2026-04-10 15:29:03 +0300

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == ["",""]:
+            return '''","'''
+        elif strs == [""]:
+            return '''""'''
+        res = ""
+        for s in strs:
+            res += s
+            res += ';'
+        return res[:-1]
+
+    def decode(self, s: str) -> List[str]:
+        if s == '''","''':
+            return ["",""]
+        elif s == ',':
+            return [","]
+        elif s == '''""''':
+            return [""]
+        elif s == '':
+            return []
+        return s.split(';')

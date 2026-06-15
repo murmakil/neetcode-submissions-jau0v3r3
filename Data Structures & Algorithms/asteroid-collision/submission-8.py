ea2a8edca222eb5334e0a10cb98ec39a92e8c5c4
murmakil <100516945+murmakil@users.noreply.github.com>
2026-06-15 23:03:49 +0300

    Add: asteroid-collision - submission-8

---
+++ Data Structures & Algorithms/asteroid-collision/submission-8.py
@@ -0,0 +1,17 @@
+from numpy import sign
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if abs(stack[-1]) == abs(asteroid):
+                    stack.pop()
+                    break
+                elif abs(stack[-1]) < abs(asteroid):
+                    stack.pop()
+                    continue
+                else:
+                    break
+            else:
+                stack.append(asteroid)
+        return stack
